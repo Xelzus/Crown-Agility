c9a6443cd9a11cb4de111e38219bdcbc5339e963
Xelzus <esaned@hotmail.com>
2016-07-19 21:07:07 -0400

    Adding reminders now works
    Not yet persistent, also added mitchell changes.

--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,30 @@
 (function () {
-    var app = angular.module('reminder', []);
-    app.controller('reminderController', function () {
-
+    var app = angular.module('dashboardApp', []);
+    app.controller('dashController', function () {
+        this.contents = reminderArray;
     });
+
+    app.controller('reminderController', function () {
+        this.reminder = {};
+
+        this.addReminder = function (content) {
+            reminderArray.push(this.reminder);
+            this.reminder = {};
+        }
+    });
+
+    var reminderArray = [
+        {
+            title: 'meeting with school counselor'
+            , description: 'sat scores'
+            , date: '12/31/2016'
+        },
+
+        {
+            title: 'meeting with school teacher'
+            , description: 'act scores'
+            , date: '12/31/2014'
+        }
+    ];
+
 })();
