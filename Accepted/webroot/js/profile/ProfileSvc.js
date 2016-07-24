8f53a51ff279b179e5bcc04dfb54bc7e912f3cb6
ctalave1 <talavera.christopher@yahoo.com>
2016-07-24 18:21:53 -0400

    CakePHP Integration
    Added CakePHP based version of the site with changes in GitHub up to
    7/24/16

---
+++ Accepted/webroot/js/profile/ProfileSvc.js
@@ -0,0 +1,35 @@
+'use strict';
+
+app.factory('profileSvc', function ($http) {
+    return {
+        loadReminders: function() {
+            return $http.get('getReminders', {
+                headers:
+                {
+                  'Content-Type' : 'application/json; charset=utf-8',
+                  'Accept' : 'application/json'
+                }
+            });
+        },
+
+        addReminder: function(reminder) {
+            return $http.post('addReminder', reminder, {
+              headers:
+              {
+                  'Content-Type' : 'application/json; charset=utf-8',
+                  'Accept' : 'application/json'
+              }
+            });
+        },
+
+        deleteReminder: function(reminderId) {
+            return $http.post('deleteReminder', reminderId, {
+              headers:
+              {
+                  'Content-Type' : 'application/json; charset=utf-8',
+                  'Accept' : 'application/json'
+              }
+            });
+        }
+    }
+});
