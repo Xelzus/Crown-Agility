05c06af0d999c3d54920757ce109560f14ddbcca
Mitch <MEnfield@fau.edu>
2016-07-25 13:05:45 -0400

    Updating edit profile page
    Updated edit profile page, added the link to edit profile into the
    landing page.

--- js/editProfile.js
+++ js/editProfile.js
@@ -1,21 +1,32 @@
 (function () {
-var app = angular.module('editProfileApp', []);   
+var app = angular.module('editProfileApp', []); 
+    app.controller('displayController', function($scope){
+       var email=$scope.email;
+        
+    });
+    
     app.controller('myCtrl', function($scope) {
         
         
         $scope.email= "Someone@something.com";
         $scope.age= "22";
         $scope.sex="male";
-        $scope.school= "Florida Atlantic University";    
+        $scope.school= "Florida Atlantic University";
+        
+        $scope.editEmail=true;
+        $scope.editAge=true;
+        $scope.editSchool=true;
         
         $scope.saveEmail=function()
         {
             
         }
+        
         $scope.saveAge=function()
         {
             
         }
+        
         $scope.saveSchool=function()
         {
             
